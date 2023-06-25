8f1780b912a47a3346dfd8864651760df323ba51
Mike <Anh.mike.pham@gmail.com>
2023-06-25 18:11:54 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,35 +1,260 @@
-import io
 import os
+from dataclasses import dataclass
+from pathlib import Path
 
+from fastapi import FastAPI
+
+from modal import Image, Mount, Secret, SharedVolume, Stub, asgi_app, method
 import modal
 
-stub = modal.Stub()
+web_app = FastAPI()
+assets_path = Path(__file__).parent / "assets"
+stub = Stub(name="example-dreambooth-app" )
 
+GIT_SHA = "ed616bd8a8740927770eebe017aedb6204c6105f"
+
+image = (
+    Image.debian_slim(python_version="3.10")
+    .pip_install(
+        "accelerate",
+        "datasets",
+        "ftfy",
+        "gradio~=3.10",
+        "smart_open",
+        "transformers",
+        "torch",
+        "torchvision",
+        "triton",
+    )
+    .pip_install("xformers", pre=True)
+    .apt_install("git")
+    # Perform a shallow fetch of just the target `diffusers` commit, checking out
+    # the commit in the container's current working directory, /root. Then install
+    # the `diffusers` package.
+    .run_commands(
+        "cd /root && git init .",
+        "cd /root && git remote add origin https://github.com/huggingface/diffusers",
+        f"cd /root && git fetch --depth=1 origin {GIT_SHA} && git checkout {GIT_SHA}",
+        "cd /root && pip install -e .",
+    )
+)
+
+volume = SharedVolume().persist("dreambooth-finetuning-vol")
+MODEL_DIR = Path("/model")
+
+@dataclass
+class SharedConfig:
+    """Configuration information shared across project components."""
+
+    # The instance name is the "proper noun" we're teaching the model
+    instance_name: str = "Qwerty"
+    # That proper noun is usually a member of some class (person, bird),
+    # and sharing that information with the model helps it generalize better.
+    class_name: str = "Golden Retriever"
+
+
+@dataclass
+class TrainConfig(SharedConfig):
+    """Configuration for the finetuning step."""
+
+    # training prompt looks like `{PREFIX} {INSTANCE_NAME} the {CLASS_NAME} {POSTFIX}`
+    prefix: str = "a photo of"
+    postfix: str = ""
+
+    # locator for plaintext file with urls for images of target instance
+    instance_example_urls_file: str = str(
+        Path(__file__).parent / "instance_example_urls.txt"
+    )
+
+    # identifier for pretrained model on Hugging Face
+    model_name: str = "runwayml/stable-diffusion-v1-5"
+
+    # Hyperparameters/constants from the huggingface training example
+    resolution: int = 512
+    train_batch_size: int = 1
+    gradient_accumulation_steps: int = 1
+    learning_rate: float = 2e-6
+    lr_scheduler: str = "constant"
+    lr_warmup_steps: int = 0
+    max_train_steps: int = 600
+    checkpointing_steps: int = 1000
+
+
+@dataclass
+class AppConfig(SharedConfig):
+    """Configuration information for inference."""
+
+    num_inference_steps: int = 50
+    guidance_scale: float = 7.5
+
+
+IMG_PATH = Path("/img")
+
+
+def load_images(image_urls):
+    import PIL.Image
+    from smart_open import open
+
+    os.makedirs(IMG_PATH, exist_ok=True)
+    for ii, url in enumerate(image_urls):
+        with open(url, "rb") as f:
+            image = PIL.Image.open(f)
+            image.save(IMG_PATH / f"{ii}.png")
+    print("Images loaded.")
+
+    return IMG_PATH
 
 @stub.function(
-    image=modal.Image.debian_slim().pip_install("torch", "diffusers[torch]", "transformers", "ftfy"),
-    secret=modal.Secret.from_name("my-huggingface-secret"),
-    gpu="any",
+    image=image,
+    gpu="A100",  # finetuning is VRAM hungry, so this should be an A100
+    shared_volumes={
+        str(
+            MODEL_DIR
+        ): volume,  # fine-tuned model will be stored at `MODEL_DIR`
+    },
+    timeout=1800,  # 30 minutes
+    # secrets=[Secret.from_name("huggingface")],
+    secret=modal.Secret.from_name("my-huggingface-secret") 
 )
-async def run_stable_diffusion(prompt: str):
-    from diffusers import StableDiffusionPipeline
+def train(instance_example_urls):
+    import subprocess
 
-    pipe = StableDiffusionPipeline.from_pretrained(
-        "runwayml/stable-diffusion-v1-5",
-        use_auth_token=os.environ["HUGGINGFACE_TOKEN"],
-    ).to("cuda")
+    import huggingface_hub
+    from accelerate.utils import write_basic_config
+    from transformers import CLIPTokenizer
 
-    image = pipe(prompt, num_inference_steps=10).images[0]
+    # set up TrainConfig
+    config = TrainConfig()
 
-    buf = io.BytesIO()
-    image.save(buf, format="PNG")
-    img_bytes = buf.getvalue()
+    # set up runner-local image and shared model weight directories
+    img_path = load_images(instance_example_urls)
+    os.makedirs(MODEL_DIR, exist_ok=True)
 
-    return img_bytes
+    # set up hugging face accelerate library for fast training
+    write_basic_config(mixed_precision="fp16")
+
+    # authenticate to hugging face so we can download the model weights
+    hf_key = os.environ["HUGGINGFACE_TOKEN"]
+    huggingface_hub.login(hf_key)
+
+    # check whether we can access to model repo
+    try:
+        CLIPTokenizer.from_pretrained(config.model_name, subfolder="tokenizer")
+    except OSError as e:  # handle error raised when license is not accepted
+        license_error_msg = f"Unable to load tokenizer. Access to this model requires acceptance of the license on Hugging Face here: https://huggingface.co/{config.model_name}."
+        raise Exception(license_error_msg) from e
+
+    # define the training prompt
+    instance_phrase = f"{config.instance_name} {config.class_name}"
+    prompt = f"{config.prefix} {instance_phrase} {config.postfix}".strip()
+
+    # run training -- see huggingface accelerate docs for details
+    subprocess.run(
+        [
+            "accelerate",
+            "launch",
+            "examples/dreambooth/train_dreambooth.py",
+            "--train_text_encoder",  # needs at least 16GB of GPU RAM.
+            f"--pretrained_model_name_or_path={config.model_name}",
+            f"--instance_data_dir={img_path}",
+            f"--output_dir={MODEL_DIR}",
+            f"--instance_prompt='{prompt}'",
+            f"--resolution={config.resolution}",
+            f"--train_batch_size={config.train_batch_size}",
+            f"--gradient_accumulation_steps={config.gradient_accumulation_steps}",
+            f"--learning_rate={config.learning_rate}",
+            f"--lr_scheduler={config.lr_scheduler}",
+            f"--lr_warmup_steps={config.lr_warmup_steps}",
+            f"--max_train_steps={config.max_train_steps}",
+            f"--checkpointing_steps={config.checkpointing_steps}",
+        ],
+        check=True,
+    )
+
+@stub.cls(
+    image=image,
+    gpu="A100",
+    shared_volumes={str(MODEL_DIR): volume},
+)
+class Model:
+    def __enter__(self):
+        import torch
+        from diffusers import DDIMScheduler, StableDiffusionPipeline
+
+        # set up a hugging face inference pipeline using our model
+        ddim = DDIMScheduler.from_pretrained(MODEL_DIR, subfolder="scheduler")
+        pipe = StableDiffusionPipeline.from_pretrained(
+            MODEL_DIR,
+            scheduler=ddim,
+            torch_dtype=torch.float16,
+            safety_checker=None,
+        ).to("cuda")
+        pipe.enable_xformers_memory_efficient_attention()
+        self.pipe = pipe
+
+    @method()
+    def inference(self, text, config):
+        image = self.pipe(
+            text,
+            num_inference_steps=config.num_inference_steps,
+            guidance_scale=config.guidance_scale,
+        ).images[0]
+
+        return image
+
+@stub.function(
+    image=image,
+    concurrency_limit=3,
+    mounts=[Mount.from_local_dir(assets_path, remote_path="/assets")],
+    secret=modal.Secret.from_name("my-huggingface-secret") 
+)
+@asgi_app()
+def fastapi_app():
+    import gradio as gr
+    from gradio.routes import mount_gradio_app
+
+    # Call to the GPU inference function on Modal.
+    def go(text):
+        return Model().inference.call(text, config)
+
+    # set up AppConfig
+    config = AppConfig()
+
+    instance_phrase = f"{config.instance_name} the {config.class_name}"
+
+    example_prompts = [
+        f"{instance_phrase}",
+        f"a painting of {instance_phrase.title()} With A Pearl Earring, by Vermeer",
+        f"oil painting of {instance_phrase} flying through space as an astronaut",
+        f"a painting of {instance_phrase} in cyberpunk city. character design by cory loftis. volumetric light, detailed, rendered in octane",
+        f"drawing of {instance_phrase} high quality, cartoon, path traced, by studio ghibli and don bluth",
+    ]
+
+    modal_docs_url = "https://modal.com/docs/guide"
+    modal_example_url = f"{modal_docs_url}/ex/dreambooth_app"
+
+    description = f"""Describe what they are doing or how a particular artist or style would depict them. Be fantastical! Try the examples below for inspiration.
+
+### Learn how to make your own [here]({modal_example_url}).
+    """
+
+    # add a gradio UI around inference
+    interface = gr.Interface(
+        fn=go,
+        inputs="text",
+        outputs=gr.Image(shape=(512, 512)),
+        title=f"Generate images of {instance_phrase}.",
+        description=description,
+        examples=example_prompts,
+        css="/assets/index.css",
+        allow_flagging="never",
+    )
+
+    # mount for execution on Modal
+    return mount_gradio_app(
+        app=web_app,
+        blocks=interface,
+        path="/",
+    )
 
 
-@stub.local_entrypoint()
-def main():
-    img_bytes = run_stable_diffusion.call("Kim Jung Un supreme leader in Roman emperor clothes and crown")
-    with open("./tmp/output.png", "wb") as f:
-        f.write(img_bytes)
